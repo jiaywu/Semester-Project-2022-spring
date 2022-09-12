f16dc872fe0d9ba8b998165421c923fcc480a8e3
LuckHYI <jiayi.wu@tum.de>
2022-09-12 16:14:45 +0200

    tiny modification

--- test.py
+++
@@ -1,8 +0,0 @@
-import torch
-import csv
-import numpy as np
-
-[training_channel_instances, cv_channel_instances, test_channel_instances] = torch.utils.data.DataLoader(
-        torch.load('channel_instances_N50_length1e4.pt'), pin_memory=False)
-
-torch.save([test_channel_instances], 'channel_instances_N50_fd_length1e4.pt')
